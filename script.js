b99d310973316546f4f5f73b83dfdd15a1382f9e
pradeepchandpenugonda <pradeepinfo478@gmail.com>
2021-09-23 16:07:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//assign the variables
+
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+jokeBtn.addEventListener('click',generateJoke)
+
+async function generateJoke(){
+    const config = {
+        headers:{
+            Accept: 'application/json'
+        }
+    }
+    const response = await fetch('https://icanhazdadjoke.com', config);
+    const data = await response.json();
+    jokeEl.innerHTML = data.joke;
+}
